db6b05015a171a3764b84a675e705cea4258054b
JXMIE95 <jamie.clark95@googlemail.com>
2025-07-27 21:36:34 +0100

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,20 +1,23 @@
 
-const { Events } = require('discord.js');
-const scheduler = require('../systems/scheduler');
 const suggestions = require('../systems/suggestions');
-const help = require('../systems/help');
-const logger = require('../utils/logger');
+const scheduler = require('../systems/scheduler');
+const { Events } = require('discord.js');
 
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
     try {
-      if (interaction.isChatInputCommand()) {
+      if (interaction.isCommand()) {
         const command = interaction.client.commands.get(interaction.commandName);
         if (!command) return;
-
         await command.execute(interaction);
-      } else if (interaction.isButton()) {
+      }
+
+      if (interaction.isButton()) {
+        if (interaction.customId.startsWith('suggest_')) {
+          await suggestions.handleSuggestionButton(interaction);
+        }
+
         if (interaction.customId.startsWith('roster_add_')) {
           await scheduler.handleRosterAdd(interaction);
         } else if (interaction.customId.startsWith('roster_cancel_')) {
@@ -27,22 +30,30 @@ module.exports = {
           await scheduler.handleRosterCancelSelection(interaction);
         } else if (interaction.customId.startsWith('shift_checkin_')) {
           await scheduler.handleShiftCheckin(interaction);
-        } else if (interaction.customId.startsWith('help_')) {
-          await help.handleHelp(interaction);
         }
-      } else if (interaction.isModalSubmit()) {
-        if (interaction.customId.startsWith('roster_add_modal_')) {
+      }
+
+      if (interaction.isModalSubmit()) {
+        if (interaction.customId.startsWith('suggest_modal')) {
+          await suggestions.handleSuggestionModal(interaction);
+        }
+
+        if (interaction.customId.startsWith('roster_add_modal')) {
           await scheduler.handleRosterAddModal(interaction);
-        } else if (interaction.customId.startsWith('roster_edit_modal_')) {
+        }
+
+        if (interaction.customId.startsWith('roster_edit_modal')) {
           await scheduler.handleRosterEditModal(interaction);
         }
-      } else if (interaction.isStringSelectMenu()) {
-        if (interaction.customId.startsWith('roster_time_select_')) {
+      }
+
+      if (interaction.isStringSelectMenu()) {
+        if (interaction.customId.startsWith('roster_time_select')) {
           await scheduler.handleRosterTimeSelect(interaction);
         }
       }
     } catch (error) {
-      logger.error('Uncaught Exception:', error);
+      console.error('[ERROR] Uncaught Exception:', error);
     }
   }
 };
